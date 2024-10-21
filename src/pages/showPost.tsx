4684034773cefe307b457a344901b546d8f4a19b
murilovantini <m.henrique.vantini18@gmail.com>
2024-10-21 13:52:15 -0300

    style: adicionar condição de visibilidade dos comentários através de variável booleana

--- src/pages/showPost.tsx
+++ src/pages/showPost.tsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 import myAxios from "@/api/axiosInstance";
 import PostModel from "@/models/postModel";
 import CircularLoading from "@/components/loading/circularLoading";
+import { ArrowDown, Eye, Heart, UserCircle } from "lucide-react";
 
 const ShowPost = () => {
   //Captura id da rota /post/:id
   const { id } = useParams();
   const [post, setPost] = useState<PostModel | null>(null);
   const [loading, setLoading] = useState(true);
+  const [commentVisibility, setCommentVisibility] = useState(false);
   const [error, setError] = useState("");
   const token = localStorage.getItem("token");
   useEffect(() => {
@@ -21,7 +23,6 @@ const ShowPost = () => {
         if (response.status !== 200) {
           throw new Error("Erro ao buscar post");
         }
-        console.log(response.data.data);
         setPost(response.data.data);
       } catch (error) {
         setError(error as string);
@@ -38,36 +39,64 @@ const ShowPost = () => {
   if (error) return <div>Erro: {error}</div>;
 
   return (
-    <div className="my-40 mx-40 p-5 border border-gray-300 rounded-md">
-      <h1 className="uppercase">{post?.title}</h1>
-      <h4>{post?.description}</h4>
-      <br />
-      {post?.chapters.length == 0 ? (
-        <div>Não possui capítulos</div>
-      ) : (
-        post?.chapters.map((chapter) => (
-          <div key={chapter.id}>
-            <h5>{chapter.title}</h5>
-            <p>{chapter.content}</p>
-          </div>
-        ))
-      )}
-      {post?.comments.length == 0 ? (
-        <div>Não possui comentários</div>
-      ) : (
-        <div>
-          <br />
-          Comentários
-          {post?.comments.map((comment) => (
-            <div key={comment.id}>
-              <h5>{comment.description}</h5>
+    <>
+      <div className="my-40 mx-40 p-5 border border-gray-300 rounded-md">
+        <h1 className="uppercase">{post?.title}</h1>
+        <h4>{post?.description}</h4>
+        {post?.image != null ? <img src={post?.image} /> : null}
+        <br />
+        {post?.chapters.length == 0 ? (
+          <div>Não possui capítulos</div>
+        ) : (
+          post?.chapters.map((chapter) => (
+            <div key={chapter.id}>
+              <h5>{chapter.title}</h5>
+              <p>{chapter.content}</p>
             </div>
-          ))}
+          ))
+        )}
+        {post?.comments.length == 0 ? (
+          <div>Não possui comentários</div>
+        ) : (
+          <div>
+            <br />
+            <div
+              className="flex flex-row gap-2 mb-2 p-2 w-32 cursor-pointer hover:bg-gray-300 rounded-md"
+              onClick={() => setCommentVisibility(!commentVisibility)}
+            >
+              <span>Comentários</span> <ArrowDown />
+            </div>
+
+            {commentVisibility
+              ? post?.comments.map((comment) => (
+                  <div
+                    key={comment.id}
+                    className="flex flex-row gap-2 items-center mb-2 border border-gray-300 p-2 rounded-md"
+                  >
+                    <UserCircle /> <h5>{comment.description}</h5>
+                  </div>
+                ))
+              : null}
+          </div>
+        )}
+        <br />
+        <div className="flex flex-col items-end">
+          <div className="flex flex-row items-end justify-end">
+            <a className="pr-1">
+              <Heart strokeWidth={1.5} />
+            </a>
+            {post?.like_posts_count}
+          </div>
+          <div className="flex flex-row items-end justify-end">
+            <a className="pr-1">
+              <Eye strokeWidth={1.5} />
+            </a>
+            {post?.access_count}
+          </div>
         </div>
-      )}
-      <br />
-      {post?.like_posts_count} curtidas
-    </div>
+        <br />
+      </div>
+    </>
   );
 };
 
