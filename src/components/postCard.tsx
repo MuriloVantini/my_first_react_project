7abc0a7ddb79d600f5c203510af42454c0190b45
murilovantini <m.henrique.vantini18@gmail.com>
2024-10-21 13:08:53 -0300

    refactor: criar componente para post na homepage

---
+++ src/components/postCard.tsx
@@ -0,0 +1,37 @@
+import PostModel from "@/models/postModel";
+import { Button } from "./ui/button";
+import { Eye } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+const PostCard = (post: PostModel) => {
+  const navigate = useNavigate();
+
+  const onNavigate = (id: number) => {
+    navigate(`/post/${id}`); // Navega para a rota de detalhes do post
+  };
+  return (
+    <div
+      key={post.id}
+      className="max-w-sm rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4"
+    >
+      <a href="#">
+        <img className="rounded-t-lg" src={post.image} alt="" />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {post.title}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {post.description}
+        </p>
+        <Button onClick={() => onNavigate(post.id)} variant="outline">
+          <Eye /> Ver post
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,10 @@
 import myAxios from "@/api/axiosInstance";
 import CircularLoading from "@/components/loading/circularLoading";
-import { Button } from "@/components/ui/button";
+import PostCard from "@/components/postCard";
 import PostModel from "@/models/postModel";
-import { Eye } from "lucide-react";
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  const navigate = useNavigate();
   const [posts, setPosts] = useState<PostModel[]>([]); // Estado para armazenar os dados
   const [loading, setLoading] = useState(true); // Estado para indicar carregamento
   const [error, setError] = useState(""); // Estado para erros
@@ -33,38 +30,15 @@ const HomePage = () => {
     fetchData(); 
   }, []); 
 
-  if (loading) return <CircularLoading url={myAxios.defaults.baseURL + "/post"}/>;
-  if (error) return <div>Erro: {error}</div>;
+  if (loading) return <CircularLoading url={myAxios.defaults.baseURL + "/post"}/>
+  if (error) return <div>Erro: {error}</div>
 
-  const onNavigate = (id: number) => {
-    navigate(`/post/${id}`); // Navega para a rota de detalhes do post
-  };
 
   return (
     <div className="py-40">
       <div className="flex flex-wrap justify-center gap-4">
         {posts.map((item) => (
-          <div
-            key={item.id}
-            className="max-w-sm rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4"
-          >
-            <a href="#">
-              <img className="rounded-t-lg" src={item.image} alt="" />
-            </a>
-            <div className="p-5">
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  {item.title}
-                </h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {item.description}
-              </p>
-              <Button onClick={() => onNavigate(item.id)} variant="outline">
-                <Eye /> Ver post
-              </Button>
-            </div>
-          </div>
+          <PostCard {...item}/>
         ))}
       </div>
     </div>
