c07731e54a65f04f9beec7e237cd04d57f5cc266
murilovantini <m.henrique.vantini18@gmail.com>
2024-10-22 14:15:42 -0300

    feat: criar toaster padrão da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import "./index.css";
 import Navbar from "./components/navbar/navbar";
 import { Outlet } from "react-router-dom";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
+  const toastOptions = {
+    closeButton:true,
+  }
+
   return (
     <>
       <Navbar />
       <Outlet />
+      <Toaster toastOptions={toastOptions} duration={3500} theme="light" position="top-right" visibleToasts={5} />
     </>
   );
 }
