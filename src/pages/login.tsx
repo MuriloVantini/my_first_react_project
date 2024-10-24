a14fee70f31ace5be2e6f409c847111957070aa9
murilovantini <m.henrique.vantini18@gmail.com>
2024-10-24 12:00:38 -0300

    feat: criar toaster dinâmico para login com bloqueio de repetição de submit

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { useNavigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { toast, Toaster } from "sonner";
 import {
   Form,
   FormControl,
@@ -13,6 +14,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import myAxios from "@/api/axiosInstance";
+import { Loader2 } from "lucide-react";
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -31,63 +33,95 @@ const LoginPage = () => {
     },
   });
 
-  async function onSubmit(values: z.infer<typeof formSchema>) {
-    try {
-      const result = await myAxios.post("/login", values);
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    let error: string = "";
+    // Chamada assíncrona para login
+    return new Promise((resolve, reject) => {
+      toast.promise(myAxios.post("/login", values), {
+        loading: "Carregando...",
+        success: (data) => {
+          setTimeout(() => {}, 1000);
+          localStorage.setItem("token", data.data.token);
+          resolve(data);
+          return "Credenciais corretas.";
+        },
+        error: (er) => {
+          form.setError("email", {
+            type: "value",
+            message: er.response.data.message,
+          });
+          form.setError("password", {
+            type: "value",
+            message: er.response.data.message,
+          });
+          reject(er);
+          error = "Erro ao efetuar login.";
+          return error;
+        },
+        finally: () => {
+          if (error.length === 0) {
+            setTimeout(() => navigate("/"), 500);
+          }
+        },
+      });
+    });
+  };
 
-      if (result.data.token !== "") {
-        // Navega para a dashboard se o login for bem-sucedido
-        localStorage.setItem("token", result.data.token);
-        navigate("/");
-      }
-    } catch (error: any) {
-      console.error("Erro na autenticação:", error);
-      // Configura o erro no formulário
-      form.setError("email", {
-        type: "value",
-        message: error.response.data.message,
-      });
-      form.setError("password", {
-        type: "value",
-        message: error.response.data.message,
-      });
-    }
-  }
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="space-y-8 py-44 px-72"
-      >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="email@email.com" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 py-44 px-72"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="email@email.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Senha</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="*******" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {!form.formState.isSubmitting ? (
+            <Button type="submit">Entrar</Button>
+          ) : (
+            <Button disabled>
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              Carregando
+            </Button>
           )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Senha</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="*******" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Entrar</Button>
-      </form>
-    </Form>
+        </form>
+      </Form>
+      <Toaster
+        richColors
+        duration={3500}
+        theme="light"
+        position="top-right"
+        visibleToasts={5}
+      />
+    </>
   );
 };
 
