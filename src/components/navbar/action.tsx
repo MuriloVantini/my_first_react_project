15ab446be26344017f5d0ab4685142ea4e714c8c
murilovantini <m.henrique.vantini18@gmail.com>
2024-10-30 13:18:23 -0300

    fix: alterar paginação da navbar para utilizar navigate do router-dom

--- src/components/navbar/action.tsx
+++ src/components/navbar/action.tsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 interface Params {
   name:string,
   linkto:string
 }
 const ActionButton = ({params}:{params:Params}) => {
+  const navigate = useNavigate();
   return (
-    <a href={params.linkto} className="hover:bg-white py-1 px-2 rounded-sm hover:cursor-pointer"> {params.name}</a>
+    <a onClick={()=> navigate(params.linkto)} className="hover:bg-white py-1 px-2 rounded-sm hover:cursor-pointer"> {params.name}</a>
   )
 }
 
